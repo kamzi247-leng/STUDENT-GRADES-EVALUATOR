9dca1851e057406f44b9ac7e78aaddf8f4d354b4
Kamsi <kamzi247@gmail.com>
2026-01-02 11:14:30 +0100

    Created a new repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# STUDENT GRADE EVALUATOR
+This program is meant to evalaute the grade of a astudent's score.
+The grade system here is the one used in Nigeria.
+
+## Concepts
+If, elif, else statements were used.
+Iputs for name, course name and scores, to get student info and grade for this project.
+
+## REFLECTION
+This programme was not hard, but it used key concepts that I've learned as a beginner.
---
+++ grade_evaluator.py
@@ -0,0 +1,23 @@
+# This is a simple grade evaluator for students
+name = input("Enter Student name: ")
+course_name = input("Enter Course name: ")
+score = float(input("Enter Student score(0-100): "))
+print("")
+
+print(f"Student Name: {name}")
+print(f"Course Name: {course_name}")
+
+if score >= 70:
+    print("Grade: A")
+elif 60 <= score < 69:
+    print("Grade: B")
+elif 50 <= score < 59:
+    print("Grade: C")
+elif 40 <= score < 49:
+    print("Grade: D")
+elif 30 <= score < 39:
+    print("Grade: E")
+else:
+    print("Grade: F")
+
+    #This is the Nigerian Federal University of Technology, Owerri grading system.
